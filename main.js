bd62a0f0f740cddf19e7249f5b4e81b1eadd869c
priyashejul43-source <priyashejul43@gmail.com>
2025-12-09 22:39:25 +0530

    added first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function login() {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+
+  if (username === "admin" && password === "1234") {
+    document.getElementById("message").innerHTML = "Login Successful!";
+  } else {
+    document.getElementById("message").innerHTML = "Invalid Credentials!";
+  }
+}
